ba7e74300b9370f7bf26848ae66aba3adf6b232d
Danny Wallace <contact@dannywallace.dev>
2022-12-11 22:44:18 -0600

    Updated cell borders

--- src/Components/CellVis.jsx
+++ src/Components/CellVis.jsx
@@ -1,10 +1,10 @@
 export default function Cell({ cell, distance }) {
   const color = 255 - distance * 3;
   const walls = {
-    borderLeft: `${cell.westWall ? '0.125em solid black' : '0'}`,
-    borderRight: `${cell.eastWall ? '0.125em solid black' : '0'}`,
-    borderTop: `${cell.northWall ? '0.125em solid black' : '0'}`,
-    borderBottom: `${cell.southWall ? '0.125em solid black' : '0'}`,
+    borderLeft: `${cell.westWall ? '4px solid black' : '0'}`,
+    borderRight: `${cell.eastWall ? '4px solid black' : '0'}`,
+    borderTop: `${cell.northWall ? '4px solid black' : '0'}`,
+    borderBottom: `${cell.southWall ? '4px solid black' : '0'}`,
   }
   const backgroundColor = {
     backgroundColor: `rgb(${color}, ${color + 20}, ${color + 35})`,
